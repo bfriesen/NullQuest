867e6b4ca35817e11ecea27b02eefa973801f325
Nathan Grant <nathan.grant@gmail.com>
2013-04-25 21:06:39 -0400

    this encryption scheme actually works as opposed to the last one which only worked until the app restarted.

--- NullQuest/Data/AESFileAccess.cs
+++ NullQuest/Data/AESFileAccess.cs
@@ -15,6 +15,15 @@ namespace NullQuest.Data
             _fileAccess = fileAccess;
         }
 
+        private Rijndael GetAlgorithm()
+        {
+            Rijndael algo = Rijndael.Create();
+            algo.Padding = PaddingMode.PKCS7;
+            algo.Key = Convert.FromBase64String("LEWHRsdfhewrkh34lkRHah==");
+            algo.IV = new byte[] { 43, 125, 183, 126, 71, 164, 226, 45, 246, 20, 161, 9, 71, 126, 120, 219 };
+            return algo;
+        }
+
         public bool DoesFileExist
         {
             get { return _fileAccess.DoesFileExist; }
@@ -22,16 +31,19 @@ namespace NullQuest.Data
 
         public System.IO.StreamReader CreateReader()
         {
-            Rijndael algorithm = Rijndael.Create();
-            CryptoStream cs = new CryptoStream(_fileAccess.CreateReader().BaseStream, algorithm.CreateDecryptor(), CryptoStreamMode.Read);
-            return new System.IO.StreamReader(cs);
+            using (Rijndael algorithm = GetAlgorithm())
+            {
+                CryptoStream cs = new CryptoStream(_fileAccess.CreateReader().BaseStream, algorithm.CreateDecryptor(), CryptoStreamMode.Read);
+                return new System.IO.StreamReader(cs);
+            }
         }
 
         public System.IO.StreamWriter CreateWriter()
         {
-            Rijndael algorithm = Rijndael.Create();
-            CryptoStream cs = new CryptoStream(_fileAccess.CreateWriter().BaseStream, algorithm.CreateEncryptor(), CryptoStreamMode.Write);
-            return new System.IO.StreamWriter(cs);
+            using (Rijndael algorithm = GetAlgorithm()) {
+                CryptoStream cs = new CryptoStream(_fileAccess.CreateWriter().BaseStream, algorithm.CreateEncryptor(), CryptoStreamMode.Write);
+                return new System.IO.StreamWriter(cs);
+            }
         }
     }
 }
